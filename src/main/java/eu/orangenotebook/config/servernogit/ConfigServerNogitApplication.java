b2219b5562947631c07f696e61a6552d50c9f1d8
Moral <moralpoczta@gmail.com>
2024-01-19 11:53:45 +0100

    Initial commit.

---
+++ src/main/java/eu/orangenotebook/config/servernogit/ConfigServerNogitApplication.java
@@ -0,0 +1,13 @@
+package eu.orangenotebook.config.servernogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerNogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerNogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/orangenotebook/config/servernogit/ConfigServerNogitApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.orangenotebook.config.servernogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerNogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
